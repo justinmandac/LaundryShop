f2354cf11d8c22872f16f464899f2e392dce2e4e
justinmandac <justinmandac@gmail.com>
2013-05-24 01:36:07 +0800

    Created Item model.

---
+++ LaundryShop/Models/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LaundryShop.Models
+{
+    // Item Model
+    // Item == Piece of clothing
+    public class Item
+    {
+        private string brand;
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        private string type;
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+    }
+}
